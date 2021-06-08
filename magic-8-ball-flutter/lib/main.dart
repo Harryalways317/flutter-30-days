27c105195b50efc78218edabaee18b42cfa57454
Harish Vadaparty <harishvadaparty@gmail.com>
2021-06-08 17:10:48 +0530

    completed

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  //const BallPage({Key? key}) : super(key: key);
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text("Ask Me Anything!"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.green,
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Image.asset("images/ball$ballNumber.png"),
+          onTap: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
