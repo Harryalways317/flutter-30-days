d61a3be54cfc8936489715f84ce78143b33b6538
Harish Vadaparty <harishvadaparty@gmail.com>
2021-06-08 16:27:36 +0530

    initial commit

--- dicee-flutter/ios/Flutter/flutter_export_environment.sh
+++ dicee-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/harishvadaparty/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/harishvadaparty/Desktop/harish/flutter/dicee-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/harishvadaparty/Desktop/harish/flutter/flutter-30-days/dicee-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/harishvadaparty/Desktop/harish/flutter/dicee-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/harishvadaparty/Desktop/harish/flutter/flutter-30-days/dicee-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -12,4 +12,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/harishvadaparty/Desktop/harish/flutter/dicee-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/harishvadaparty/Desktop/harish/flutter/flutter-30-days/dicee-flutter/.dart_tool/package_config.json"
--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,32 +17,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 4;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onTap: null,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("images/dice1.png"),
-              ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: GestureDetector(
+            onTap: () => changeDiceFace(),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("images/dice$leftDiceNumber.png"),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("images/dice$rightDiceNumber.png"),
+                  ),
+                )
+              ],
             ),
           ),
-          Expanded(
-            child: GestureDetector(
-              onTap: null,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("images/dice5.png"),
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Text(
+            "The sum is ${leftDiceNumber + rightDiceNumber}",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
