efc9c28ccc855e31365f502c9ef6e5133ef09c6f
Harish Vadaparty <harishvadaparty@gmail.com>
2021-06-11 23:26:51 +0530

    intial commit

---
+++ leave_application/lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const blue = Color.fromARGB(255, 92, 145, 249);
+const double kDefaultPadding = 20.0;
---
+++ leave_application/lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String name = "Kevin Durand";
+
+  String duration = "12 oct 2019 to 15 oct 2019";
+  String status = "Pending";
+  String status1 = "Pending";
+  String status2 = "Rejected";
+  String status3 = "Approved";
+}
